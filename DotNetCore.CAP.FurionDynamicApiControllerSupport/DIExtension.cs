d3cb60b07209bd4e0280410a20e268d742f14617
焜 <tetris@vip.qq.com>
2022-06-16 23:01:05 +0800

    增加注释

--- DotNetCore.CAP.FurionDynamicApiControllerSupport/DIExtension.cs
+++ DotNetCore.CAP.FurionDynamicApiControllerSupport/DIExtension.cs
@@ -17,7 +17,7 @@ namespace DotNetCore.CAP.FurionDynamicApiControllerSupport
         /// 注入Furion动态控制器的Cap支持 
         /// </summary>
         /// <param name="services"></param>
-        /// <param name="filter"></param>
+        /// <param name="filter">程序集过滤器</param>
         public static void AddCapSupportOfDynamicController(this IServiceCollection services, string filter = "")
         {
             services.Add(new ServiceDescriptor(typeof(IConsumerServiceSelector),
